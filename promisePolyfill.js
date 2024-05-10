fc027eedba8f9037822fe0904288eac9f6b1705b
prikshit8 <prikshitchawla.v@gmail.com>
2024-05-10 13:26:18 +0530

    promise polyfill

--- promisePolyfill.js
+++ promisePolyfill.js
@@ -1,27 +1,54 @@
 function Promise(executer) {
     let onResolve
     let onReject;
-    function resolve(value) {
-        onResolve(value);
+    let isFulfilled = false;
+    let isRejected = false;
+    let isCalled = false;
+    let value;
+    function resolve(val) {
+        isFulfilled = true;
+        value = val;
+        if (typeof onResolve === 'function') {
+            onResolve(val);
+            isCalled = true;
+        }
     }
-    function reject(value) {
-        onReject(value)
+    function reject(val) {
+        isRejected = true;
+        value = val;
+        if (typeof onReject === 'function') {
+            onReject(val);
+        }
     }
     this.then = function (callback) {
         onResolve = callback;
+        if (isFulfilled && !isCalled) {
+            isCalled = true;
+            onResolve(value);
+        }
         return this;
     }
     this.catch = function (callback) {
         onReject = callback;
+        if (isRejected && !isCalled) {
+            isCalled = true;
+            onReject(value);
+        }
         return this;
     }
-    executer(resolve, reject);
+    try {
+        executer(resolve, reject);
+    }
+    catch (error) {
+        reject(error);
+    }
+
 }
 
 const p1 = new Promise((res, rej) => {
-    // setTimeout(()=>{
-    res('100');
-    // },1000);
+    setTimeout(() => {
+        rej('100');
+    }, 1000);
 })
 
 p1.then((data) => console.log(data));
